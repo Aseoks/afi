0a6ffb5d94c1e91f23fb8943ddc8ecddf5d0ecad
Wojciech Meck <wojciechmeck@gmail.com>
2023-04-11 12:02:12 +0200

    Added new section and RWD

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 console.log('hello world');
 
-$("#menu-show").click(function(){
-    $("#navigation-menu").fadeToggle('slow');   
 
-});
+const buttonThree = document.querySelector('.button-three');
+const navMenu = document.getElementById('navigation-menu');
+
+
+buttonThree.addEventListener('click', ()=>{
+  
+const isOpened = buttonThree.getAttribute('aria-expanded');
+    
+
+    if (isOpened === 'false') {
+        buttonThree.setAttribute('aria-expanded', 'true')
+        buttonThree.style.setProperty('--button-color', 'black');
+        navMenu.style.display = 'block';        
+    } else{
+        buttonThree.setAttribute('aria-expanded', 'false')
+        buttonThree.style.setProperty('--button-color', 'white');
+        navMenu.style.display = 'none';        
+    }
+})
+
 
 $(".list-item").mouseover(function(){
     $("#selected-item").text($(this).text());
@@ -13,9 +30,6 @@ $(".scroll-down").click(function(){
     $(window).scrollTop($("body").height());
 });
 
-// $(window).scroll(function(){
-//     $(window).scrollTop(0);
-// });
-    
+
 
     
